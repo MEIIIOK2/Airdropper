78baae5f00140fd1a2c27f788fc319e794360a66
MEIIIOK2 <mishasolomin344@gmail.com>
2024-05-15 11:31:29 +0300

    upd manifest

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 const manifestUrl =
-  "https://alefmanvladimir.github.io/my-twa/tonconnect-manifest.json";
+  "https://raw.githubusercontent.com/MEIIIOK2/Airdropper/main/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
